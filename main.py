3c2c45c2a4a3478797e7cd01a995bab510592ecd
Ashikur Rahman <ashikur.jebu111@gmail.com>
2025-06-12 23:06:15 +0600

    frist commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# writing my first python code
+x=10
+y=20
+
+sum=x+y
+sub=x-y
+mul=x*y
+div=x/y
+
+print("The sum of",x,"and",y,"is",sum)
+print("The sub of",x,"and",y,"is",sub)
+print("The mul of",x,"and",y,"is",mul)
+print("The div of",x,"and",y,"is",div)
+
+"""
+The sum of 10 and 20 is 30
+The sub of 10 and 20 is -10
+The mul of 10 and 20 is 200
+The div of 10 and 20 is 0.5
+"""
